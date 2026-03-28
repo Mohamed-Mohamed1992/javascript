06d075512f44d7ce998472bd1066d2caa2030f0a
Mohamed Mohamed <250026672+Mohamed-Mohamed1992@users.noreply.github.com>
2026-03-28 17:48:02 +0000

    complete if statement excercises

---
+++ conditionals/excercise.js
@@ -0,0 +1,23 @@
+// // (If) Check if a number is positive
+
+// let number = 10;
+
+// // Your task:
+// // If the number is greater than 0, print "Positive Number"
+
+// if (number > 0) {
+//     console.log("Positive number");
+// }
+//...................................................................
+
+// //(if-else) Check if a number is even or odd
+
+// let number = 7;
+
+// if (number % 2 == 0) {
+//     console.log("Even")
+// } else {
+//     console.log("odd")
+// }
+
+//......................................................................
